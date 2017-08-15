8eadb63b1d71c6235bf26769ced7e66577854c58
Deepak Kumar <deepak.kumar2@COMVIVA.COM>
2017-08-15 12:20:53 +0530

    java 8 fucutions

--- src/main/java/javatechy/lambdas/subsitute/EmailNotifier.java
+++ src/main/java/javatechy/lambdas/subsitute/EmailNotifier.java
@@ -1,7 +1,6 @@
 package javatechy.lambdas.subsitute;
 
 class EmailNotifier implements Notifier{
-
 	@Override
 	public void notifyMe() {
 		System.out.println("I m in EmailNotifier notifying your for the mails");
--- src/main/java/javatechy/lambdas/subsitute/theory.md
+++ src/main/java/javatechy/lambdas/subsitute/theory.md
@@ -19,3 +19,4 @@ interface Notifier {
 	void notifyMe();
 }
 ```
+
