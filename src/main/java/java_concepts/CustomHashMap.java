2542353a51fa945d4ea98a9de6c2326336d96eb9
deepak.kumar <deepak@localhost>
2018-04-06 13:48:41 +0530

    Custom Hash Map Added

--- src/main/java/java_concepts/CustomHashMap.java
+++ src/main/java/java_concepts/CustomHashMap.java
@@ -1,8 +1,24 @@
 package java_concepts;
 
-public class CustomHashMap {
-	
-	
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class CustomHashMap<K, V> {
+	List<K> bucket;
+
+	CustomHashMap(int size) {
+		bucket = new ArrayList<>();
+	}
+
+	public Set<K> keySet() {
+		return null;
+	}
+
+	public List<V> values() {
+		return null;
+	}
+
 }
 
 class MyEntry<K, V> {
