69c3e5dab5d75186813509bd3336657cff1bbc5b
javatechy <deepak@192.168.1.191>
2018-10-28 17:46:42 +0530

    Kadane Algortihm Code corrected

--- src/main/java/arrays/KadaneAlgorith.java
+++ src/main/java/arrays/KadaneAlgorith.java
@@ -11,9 +11,9 @@ public class KadaneAlgorith {
 	// Driver program to test above functions
 	public static void main(String[] args) {
 
-		int ar[] = new int[] { -2, -1 };
+		int ar[] = new int[] { -1, -2,-3 };
 
-		int sum = new KadaneAlgorith().maxContigeousSum(ar);
+		int sum = new KadaneAlgorith().kadane(ar);
 		System.out.println("Max sume: " + sum);
 	}
 
@@ -37,16 +37,17 @@ public class KadaneAlgorith {
 	 * @return
 	 */
 	int kadane(int arr[]) {
-		int max = 0;
+		int max = Integer.MIN_VALUE;
 		int inter_max = 0;
 
 		for (int i = 0; i < arr.length; i++) {
 			inter_max = inter_max + arr[i];
+			if (inter_max > max) {
+				max = inter_max;//Math.max(inter_max,max);
+			}
 			if (inter_max < 0)
 				inter_max = 0;
-			if (inter_max > max) {
-				max = inter_max;
-			}
+			
 		}
 
 		return max;
